59cb0272fe33cfa31a7cd7d3dfdf324faa088c3c
Anveshi Shori <anveshi87@gmail.com>
2021-01-16 16:41:00 +0530

    pc32

---
+++ Slingshot.js
@@ -0,0 +1,42 @@
+class Slingshot{
+    constructor(body1,body2){
+    
+        var options = {
+    
+            bodyA: body1,
+            pointB: body2,
+            stiffness: 0.04,
+            length:2
+        }
+           
+        this.sling = Constraint.create(options);
+        World.add(myworld,this.sling)
+    
+    
+    
+    }
+    
+    attach(body){
+        this.sling.bodyA = body
+    }
+    
+    fly(){
+    this.sling.bodyA = null
+       
+    }
+    
+    display(){
+    
+        if (this.sling.bodyA){
+
+        var pointA = this.sling.bodyA.position
+        var pointB = this.sling.pointB
+       push()
+        stroke("white")
+        line(pointA.x, pointA.y, pointB.x,pointB.y)
+        pop();    
+    }
+    }
+    
+    
+    }
